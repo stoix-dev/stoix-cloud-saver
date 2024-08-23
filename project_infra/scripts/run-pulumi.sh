0d18343c591df63a70f67b5c2e03f56a82f8773c
Roberto1501 <roberto1501@yahoo.com>
2024-08-23 18:24:07 -0300

    adding the pulumi stack

--- project_infra/scripts/run-pulumi.sh
+++ project_infra/scripts/run-pulumi.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e -x
 
-# Add the pulumi CLI to the PATH
+# Add the Pulumi CLI to the PATH
 export PATH=$PATH:$HOME/.pulumi/bin
+
+# Navigate to the project directory and install dependencies
 cd ../
 npm install
+
+# Login to Pulumi (assuming PULUMI_ACCESS_TOKEN is already set in the environment)
 pulumi login
-pulumi stack select production
+
+# Check if the stack exists; if not, create it
+STACK_NAME="production"
+if pulumi stack select $STACK_NAME --non-interactive; then
+    echo "Stack $STACK_NAME selected."
+else
+    echo "Stack $STACK_NAME does not exist. Creating stack $STACK_NAME."
+    pulumi stack init $STACK_NAME
+fi
+
+# Update the stack (apply the changes)
 pulumi up -y
