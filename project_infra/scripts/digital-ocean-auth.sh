fba8d2554f0ff870c95d75e6f123ecdf168818a5
Roberto1501 <roberto1501@yahoo.com>
2024-08-23 17:25:32 -0300

    correcting errors

--- project_infra/scripts/digital-ocean-auth.sh
+++ project_infra/scripts/digital-ocean-auth.sh
@@ -3,7 +3,12 @@
 # Setup the DigitalOcean CLI (doctl) with the API token
 echo "Configuring DigitalOcean CLI with the secure token..."
 export DIGITALOCEAN_TOKEN=$1  # Take token as an argument
+export DIGITALOCEAN_USERNAME=$2
+export DOCKER_REGISTRY=$3
 doctl auth init --access-token $DIGITALOCEAN_TOKEN
+echo $DIGITALOCEAN_TOKEN | docker login registry.digitalocean.com -u $DIGITALOCEAN_USERNAME --password-stdin
 
 # Validate the token and set up context
 echo "Token validation and context setup complete."
+sudo docker push registry.digitalocean.com/$DOCKER_REGISTRY/stoix-cloud-saver-web-next-app
+
