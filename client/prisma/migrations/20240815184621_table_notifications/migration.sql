037c8b75820004a6451ece436ca648f763724bcb
Leonardo Menezes <leonardo@stoix.dev>
2026-07-17 17:25:35 -0300

    chore: remove legado (template NextUI, PoC hello-world, infra copiada)

--- client/prisma/migrations/20240815184621_table_notifications/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "notifications" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
-);
--- services-infra/scripts/pulumi-preview.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e -x
-
-export PATH=$PATH:$HOME/.pulumi/bin
-
-npm install
-pulumi login
-pulumi stack select homologacao
-pulumi preview
